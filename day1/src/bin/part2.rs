5d0f96d97176e7330c8265a9ad7c5ef99f26e0ef
Matt Casper <matthewvcasper@gmail.com>
2023-12-01 11:44:12 -0800

    Day 1, Part 2

--- day1/src/bin/part2.rs
+++ day1/src/bin/part2.rs
@@ -8,7 +8,70 @@ fn main() -> Result<()> {
 }
 
 fn solve(lines: Vec<String>) -> i64 {
-    0
+    let mut numbers = vec![];
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut substrs = vec![];
+
+    for line in lines {
+        let mut first_number = -1;
+        let mut last_number = -1;
+
+        let chars = line.split("");
+        for c in chars {
+            if let Ok(i) = c.parse::<i64>() {
+                if first_number == -1 {
+                    first_number = i;
+                }
+
+                last_number = i;
+                substrs.clear();
+            } else {
+                let mut new_substrs: Vec<String> =
+                    substrs.iter().map(|s| format!("{}{}", s, c)).collect();
+                new_substrs.push(c.to_owned());
+
+                substrs.clear();
+                for substr in new_substrs {
+                    let mut matched = false;
+
+                    for (i, word) in words.iter().enumerate() {
+                        if word.starts_with(&substr) {
+                            matched = true;
+                            if word == &substr {
+                                let ii = i + 1;
+
+                                if first_number == -1 {
+                                    first_number = ii as i64;
+                                }
+
+                                last_number = ii as i64;
+                                matched = false;
+                            }
+                        }
+                    }
+
+                    if matched {
+                        substrs.push(substr);
+                    }
+                }
+            }
+        }
+
+        println!(
+            "Found {} and {} for line {}",
+            first_number, last_number, line
+        );
+
+        if first_number == -1 || last_number == -1 {
+            panic!("failed to find first or last number!")
+        }
+
+        numbers.push((first_number * 10) + last_number)
+    }
+
+    numbers.iter().sum()
 }
 
 fn lines(path: String) -> Result<Vec<String>> {
@@ -28,8 +91,8 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let expected = 11111;
-        let actual = solve(lines("src/bin/sample.txt".into()).unwrap());
+        let expected = 281;
+        let actual = solve(lines("src/bin/sample2.txt".into()).unwrap());
         assert_eq!(expected, actual);
     }
 }
