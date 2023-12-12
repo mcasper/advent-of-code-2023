b11a1466d49ea50de1d0c75842a69e774b9653ce
Matt Casper <matthewvcasper@gmail.com>
2023-12-12 09:08:27 -0800

    Day 11, Part 2

--- day11/src/bin/part2.rs
+++ day11/src/bin/part2.rs
@@ -3,12 +3,72 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let lines = lines("src/bin/input.txt".into())?;
     let result = solve(lines);
-    println!("Day 11, part 2 result: {}", result);
+    println!("Day 11, part 1 result: {}", result);
     Ok(())
 }
 
+#[derive(Debug, Clone)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+}
+
 fn solve(lines: Vec<String>) -> i64 {
-    0
+    let mut galaxies: Vec<Point> = vec![];
+    let max_x = (lines.first().unwrap().len() - 1) as i64;
+    let max_y = (lines.len() - 1) as i64;
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                galaxies.push(Point::new(x as i64, y as i64))
+            }
+        }
+    }
+
+    let mut expanded_galaxies = galaxies.clone();
+    for x in 0..=max_x {
+        if galaxies.iter().all(|g| g.x != x) {
+            for (i, galaxy) in galaxies.iter().enumerate() {
+                if galaxy.x > x {
+                    expanded_galaxies[i].x += 1000000 - 1;
+                }
+            }
+        }
+    }
+
+    for y in 0..=max_y {
+        if galaxies.iter().all(|g| g.y != y) {
+            for (i, galaxy) in galaxies.iter().enumerate() {
+                if galaxy.y > y {
+                    expanded_galaxies[i].y += 1000000 - 1;
+                }
+            }
+        }
+    }
+
+    let mut pairs: Vec<(Point, Point)> = vec![];
+    for (i, g1) in expanded_galaxies.iter().enumerate() {
+        for i2 in (i + 1)..expanded_galaxies.len() {
+            let g2 = expanded_galaxies.get(i2).unwrap();
+            pairs.push((g1.clone(), g2.clone()));
+        }
+    }
+
+    let mut result = 0;
+
+    for (g1, g2) in pairs {
+        result += (g1.x - g2.x).abs();
+        result += (g1.y - g2.y).abs();
+    }
+
+    result
 }
 
 fn lines(path: String) -> Result<Vec<String>> {
@@ -20,16 +80,4 @@ fn lines(path: String) -> Result<Vec<String>> {
         .filter(|input| input != "")
         .collect();
     Ok(l)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_solve() {
-        let expected = 11111;
-        let actual = solve(lines("src/bin/sample.txt".into()).unwrap());
-        assert_eq!(expected, actual);
-    }
 }
