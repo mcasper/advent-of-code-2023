c6d977ab8c86cd3318aa88a43e3992cdb7c0f82b
Matt Casper <matthewvcasper@gmail.com>
2023-12-12 11:38:16 -0800

    Day 12, Part 2

--- day12/src/bin/part2.rs
+++ day12/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 fn main() -> Result<()> {
@@ -7,8 +9,124 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Clone, Debug)]
+struct Report {
+    springs: Vec<char>,
+    broken_segments: Vec<usize>,
+}
+
+impl From<&String> for Report {
+    fn from(value: &String) -> Self {
+        let parts = value.split(" ").collect::<Vec<&str>>();
+        Report {
+            springs: parts[0].chars().collect(),
+            broken_segments: parts[1]
+                .split(",")
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect(),
+        }
+    }
+}
+
+impl Report {
+    fn unfold(&self) -> Self {
+        let mut new_springs = self.springs.clone();
+        let mut new_segments = self.broken_segments.clone();
+        for _ in 0..4 {
+            new_springs.push('?');
+            new_springs.append(&mut self.springs.clone());
+            new_segments.append(&mut self.broken_segments.clone());
+        }
+        Report {
+            springs: new_springs,
+            broken_segments: new_segments,
+        }
+    }
+}
+
+fn possibles(
+    cache: &mut HashMap<(Vec<char>, Vec<usize>), i64>,
+    cs: Vec<char>,
+    segments: Vec<usize>,
+) -> i64 {
+    let key = (cs.clone(), segments.clone());
+
+    if let Some(answer) = cache.get(&key) {
+        return *answer;
+    }
+
+    if segments.is_empty() {
+        if cs.iter().all(|c| c != &'#') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if cs.is_empty() {
+        return 0;
+    }
+
+    if segments.first().unwrap() > &cs.len() {
+        return 0;
+    }
+
+    let ways = match (cs.first(), segments.first()) {
+        (Some('.'), _) => possibles(cache, cs[1..cs.len()].to_vec(), segments),
+        (Some('#'), Some(l)) => {
+            if cs[0..*l].iter().all(|c| c == &'#' || c == &'?')
+                && cs.get(*l).unwrap_or(&'a') != &'#'
+            {
+                let remaining = if *l >= cs.len() {
+                    vec![]
+                } else {
+                    cs[*l + 1..cs.len()].to_vec()
+                };
+                possibles(cache, remaining, segments[1..segments.len()].to_vec())
+            } else {
+                0
+            }
+        }
+        (Some('?'), Some(l)) => {
+            if cs[0..*l].iter().all(|c| c == &'#' || c == &'?')
+                && cs.get(*l).unwrap_or(&'a') != &'#'
+            {
+                let remaining = if *l >= cs.len() {
+                    vec![]
+                } else {
+                    cs[*l + 1..cs.len()].to_vec()
+                };
+
+                possibles(cache, remaining, segments[1..segments.len()].to_vec())
+                    + possibles(cache, cs[1..cs.len()].to_vec(), segments)
+            } else {
+                possibles(cache, cs[1..cs.len()].to_vec(), segments)
+            }
+        }
+        (None, _) => 0,
+        (_, None) => 0,
+        (a, b) => unreachable!("unhandled case {:?},{:?}", a, b),
+    };
+    cache.insert(key, ways);
+    ways
+}
+
 fn solve(lines: Vec<String>) -> i64 {
-    0
+    let reports: Vec<Report> = lines.iter().map(|l| l.into()).collect();
+    let unfolded: Vec<Report> = reports.iter().map(|report| report.unfold()).collect();
+    let mut configs = 0;
+    let mut cache: HashMap<(Vec<char>, Vec<usize>), i64> = HashMap::new();
+
+    for report in unfolded {
+        let count = possibles(
+            &mut cache,
+            report.springs.clone(),
+            report.broken_segments.clone(),
+        );
+        configs += count;
+    }
+
+    configs
 }
 
 fn lines(path: String) -> Result<Vec<String>> {
@@ -28,7 +146,7 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let expected = 11111;
+        let expected = 525152;
         let actual = solve(lines("src/bin/sample.txt".into()).unwrap());
         assert_eq!(expected, actual);
     }
